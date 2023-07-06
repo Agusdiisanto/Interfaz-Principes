effc9ff51f53b26471b8ee5220d0505014c5d2ce
santi <santiago_abregu_@hotmail.com>
2023-07-06 04:57:44 -0300

    Feat Mover

--- Web/principes-persia/src/App.jsx
+++ Web/principes-persia/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import Form from "./components/Crear/Form"
+import Mover from "./components/Mover/Mover"
 import Home from "./components/Home"
 import Ubicaciones from "./components/Ubicaciones/Ubicaciones"
 import Subscribe from "./components/Subscripcion/Subscribe"
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/ubicaciones" element={<Ubicaciones/>}/>
         <Route path="/crear" element={<Form/>}/>
+        <Route path="/mover" element={<Mover/>}/>
         <Route path="/subscribir" element={<Subscribe/>}/>
         <Route path="/conectados" element={<Conectados/>}/>
       </Routes>
---
+++ Web/principes-persia/src/components/Mover/Mover.jsx
@@ -0,0 +1,71 @@
+import { moverVector } from "../../services/Api";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import "./Mover.css";
+import GoBack from "../../utils/GoBack";
+
+const Mover = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [error, setErrorMessage] = useState("");
+  const onSubmit = async (data) => {
+    try {
+      await moverVector(data);
+      console.log("El vector se ha movido correctamente");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <div className="bg-container">
+        <img
+        src="https://cloudfront-us-east-1.images.arcpublishing.com/infobae/KMLUD4LV6JEE3LQWOPCTRPT7ZA.jpg"
+        alt="vectores en movimiento"
+        />
+        <div className="form-container">
+          <div className="form">
+            <h2 className="form-title">Mover vector</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-group">
+                <label htmlFor="vectorID" className="form-label">ID del vector</label>
+                <input
+                  type="text"
+                  id="vectorID"
+                  {...register("vectorID", { required: true })}
+                  className={`form-input ${errors.vectorID ? "error" : ""}`}
+                />
+                {errors.vectorID && (
+                  <p className="error-message">Este campo es requerido</p>
+                )}
+              </div>
+              <div className="form-group">
+                <label htmlFor="nombre" className="form-label">Nombre del destino</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  {...register("nombre", { required: true })}
+                  className={`form-input ${errors.nombre ? "error" : ""}`}
+                />
+                {errors.nombre && (
+                  <p className="error-message">Este campo es requerido</p>
+                )}
+              </div>
+              <div className="summit-container">
+                <button type="submit" className="submit-button">Mover vector</button>
+              </div>
+            </form>
+            {error && <p className="error-message">{error}</p>}
+          </div>
+        </div>
+      </div>
+      <GoBack />
+    </div>
+  );
+};
+
+export default Mover;
