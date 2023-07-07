9b3f71651519cc6075afce9c9625f922cdd1f87d
santi <santiago_abregu_@hotmail.com>
2023-07-07 06:08:44 -0300

    Refactor Mover & moverMasCorto

--- Web/principes-persia/src/components/Mover/Mover.jsx
+++ Web/principes-persia/src/components/Mover/Mover.jsx
@@ -1,15 +1,8 @@
 import { moverVector } from "../../services/Api";
-import { useForm } from "react-hook-form";
 import { useState } from "react";
-import "./Mover.css";
-import GoBack from "../../utils/GoBack";
+import Form from "../../utils/Form/Form";
 
 const Mover = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
   const [error, setErrorMessage] = useState("");
   const onSubmit = async (data) => {
     try {
@@ -21,51 +14,12 @@ const Mover = () => {
   };
 
   return (
-    <div className="main-container">
-      <div className="bg-container">
-        <img
-          className="mover-img"
-          src="https://cloudfront-us-east-1.images.arcpublishing.com/infobae/KMLUD4LV6JEE3LQWOPCTRPT7ZA.jpg"
-          alt="vectores en movimiento"
-        />
-        <div className="form-container">
-          <div className="form">
-            <h2 className="form-title">Mover vector</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-group">
-                <label htmlFor="vectorID" className="form-label">ID del vector</label>
-                <input
-                  type="text"
-                  id="vectorID"
-                  {...register("vectorID", { required: true })}
-                  className={`form-input ${errors.vectorID ? "error" : ""}`}
-                />
-                {errors.vectorID && (
-                  <p className="error-message">Este campo es requerido</p>
-                )}
-              </div>
-              <div className="form-group">
-                <label htmlFor="nombre" className="form-label">Nombre del destino</label>
-                <input
-                  type="text"
-                  id="ubicacionNombre"
-                  {...register("ubicacionNombre", { required: true })}
-                  className={`form-input ${errors.ubicacionNombre ? "error" : ""}`}
-                />
-                {errors.nombre && (
-                  <p className="error-message">Este campo es requerido</p>
-                )}
-              </div>
-              <div className="summit-container">
-                <button type="submit" className="submit-button">Mover vector</button>
-              </div>
-            </form>
-            {error && <p className="error-message">{error}</p>}
-          </div>
-        </div>
-      </div>
-      <GoBack />
-    </div>
+    <Form
+      title="Mover vector"
+      img="https://cloudfront-us-east-1.images.arcpublishing.com/infobae/KMLUD4LV6JEE3LQWOPCTRPT7ZA.jpg"
+      onSubmit={onSubmit}
+      error={error}
+    />
   );
 };
 
--- Web/principes-persia/src/components/MoverMasCorto/MoverMasCorto.jsx
+++ Web/principes-persia/src/components/MoverMasCorto/MoverMasCorto.jsx
@@ -1,15 +1,8 @@
 import { moverMasCorto } from "../../services/Api";
-import { useForm } from "react-hook-form";
 import { useState } from "react";
-import "./MoverMasCorto.css";
-import GoBack from "../../utils/GoBack";
+import Form from "../../utils/Form/Form";
 
 const MoverMasCorto = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
   const [error, setErrorMessage] = useState("");
   const onSubmit = async (data) => {
     try {
@@ -21,51 +14,12 @@ const MoverMasCorto = () => {
   };
 
   return (
-    <div className="main-container">
-      <div className="bg-container">
-        <img
-          className="moverMasCorto-img"
-          src="https://thumbs.dreamstime.com/b/un-animal-abstracto-que-encarna-el-concepto-de-velocidad-y-agilidad-ai-generativo-273299364.jpg"
-          alt="vectores en movimiento"
-        />
-        <div className="form-container">
-          <div className="form">
-            <h2 className="form-title">Mover por el camino mas corto</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-group">
-                <label htmlFor="vectorID" className="form-label">ID del vector</label>
-                <input
-                  type="text"
-                  id="vectorID"
-                  {...register("vectorID", { required: true })}
-                  className={`form-input ${errors.vectorID ? "error" : ""}`}
-                />
-                {errors.vectorID && (
-                  <p className="error-message">Este campo es requerido</p>
-                )}
-              </div>
-              <div className="form-group">
-                <label htmlFor="nombre" className="form-label">Nombre del destino</label>
-                <input
-                  type="text"
-                  id="nombre"
-                  {...register("nombre", { required: true })}
-                  className={`form-input ${errors.nombre ? "error" : ""}`}
-                />
-                {errors.nombre && (
-                  <p className="error-message">Este campo es requerido</p>
-                )}
-              </div>
-              <div className="summit-container">
-                <button type="submit" className="submit-button">Mover vector</button>
-              </div>
-            </form>
-            {error && <p className="error-message">{error}</p>}
-          </div>
-        </div>
-      </div>
-      <GoBack />
-    </div>
+    <Form
+      title="Mover por el camino mas corto"
+      img="https://thumbs.dreamstime.com/b/un-animal-abstracto-que-encarna-el-concepto-de-velocidad-y-agilidad-ai-generativo-273299364.jpg"
+      onSubmit={onSubmit}
+      error={error}
+    />
   );
 };
 
