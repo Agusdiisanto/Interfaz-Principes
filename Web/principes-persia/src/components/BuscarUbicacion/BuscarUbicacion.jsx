a06b7397c6999c3bf4c815dae2c151b66ad028ed
Agustin Di Santo <agusdisanto99@gmail.com>
2023-07-10 10:03:59 -0300

    Estilos de Buscar Ubicacion

--- Web/principes-persia/src/components/BuscarUbicacion/BuscarUbicacion.jsx
+++ Web/principes-persia/src/components/BuscarUbicacion/BuscarUbicacion.jsx
@@ -1,30 +1,33 @@
 import { useState, useEffect, useContext } from 'react';
 import { recuperarUbicacion } from '../../services/Api';
 import Ubicacion from '../Ubicaciones/Ubicacion';
-import "../Ubicaciones/Ubicacion.css"
+import "./BuscarUbicacion.css"
 import GoBack from '../../utils/GoBack';
 import Loader from '../../utils/Loader/Loader';
 import Searcher from '../Conectados/Searcher';
-import {UbicacionContext} from '../../context/UbicacionContext'
+import { UbicacionContext } from '../../context/UbicacionContext';
 
 const BuscarUbicacion = () => {
-  const {mensaje} = useContext(UbicacionContext)
-  const [ubicacion, setUbicacion] = useState(null); 
+  const { mensaje } = useContext(UbicacionContext);
+  const [ubicacion, setUbicacion] = useState(null);
   const [searchQuery, setSearchQuery] = useState(null);
-  const [isLoading, setIsLoading] = useState(false); 
-  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const getUbicacion = () => {
     if (searchQuery !== null) {
+      setIsLoading(true);
+      setError("");
       recuperarUbicacion(searchQuery)
         .then((response) => {
           setUbicacion(response);
-          setIsLoading(false); 
-          setError("")
         })
         .catch((error) => {
-          setUbicacion(null)
-          setError('No se ha encontrado esa ubicacion', error);
+          setUbicacion(null);
+          setError('No se ha encontrado esa ubicación');
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     }
   };
@@ -35,27 +38,26 @@ const BuscarUbicacion = () => {
     }
   }, [searchQuery]);
 
- 
   return (
     <div className="ubicaciones-container">
       {isLoading && (
         <Loader />
-        )} 
-        <Searcher setQuery={setSearchQuery} />
+      )} 
+      <Searcher setQuery={setSearchQuery} />
+      <div className="ubicacion-container">
         {ubicacion !== null && <Ubicacion ubicacion={ubicacion}/>}
-        {mensaje && searchQuery === mensaje.nombre && 
-        
-          <h2>  
-            {mensaje.alerta === "Rojo" ? "Ten mucho cuidao, tu ubicacion esta en alerta Roja hay muchos vectores" : ""}
-            {mensaje.alerta === "Amarillo" ? "Ten cuidao, tu ubicacion esta en alerta Amarilla hay vectores" : ""}
-            {mensaje.alerta === "Verde" ? "Tu ubicacion esta en Verde estas seguro" : ""}
-          </h2> 
-        
-        
-        }
-        {error && <h2 className='error-mensaje'>{error}</h2>}
+      </div>
+      {mensaje && searchQuery === mensaje.nombre && (
+        <h2 className={`mensaje-alerta mensaje-${mensaje.alerta}`}>
+          {mensaje.alerta === 'Rojo' && '¡Ten mucho cuidado! Esta ubicación se encuentra en alerta roja.'}
+          {mensaje.alerta === 'Amarillo' && '¡Ten cuidado! Esta ubicación se encuentra en alerta amarilla.'}
+          {mensaje.alerta === 'Verde' && 'Esta ubicación se encuentra en alerta verde. ¡Todo está bien!'}
+        </h2>
+      )}
+      {error && <h2 className="error-mensaje">{error}</h2>}
+      <div className="go-back-container">
         <GoBack />
-        
+      </div>
     </div>
   );
 };
