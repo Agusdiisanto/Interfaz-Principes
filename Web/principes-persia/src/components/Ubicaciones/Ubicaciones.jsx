8c332aab40b48b385f7aa6a653ef72b62b222d60
manudoro <manueldobrotinich@gmail.com>
2023-07-12 04:41:41 -0300

    ubicaciones se modifica dinamicamente

--- Web/principes-persia/src/components/Ubicaciones/Ubicaciones.jsx
+++ Web/principes-persia/src/components/Ubicaciones/Ubicaciones.jsx
@@ -1,55 +1,34 @@
 import { useState, useEffect } from 'react';
-import { obtenerUbicaciones } from '../../services/Api';
-import Ubicacion from './Ubicacion';
-import "./Ubicacion.css"
-import GoBack from '../../utils/GoBack';
-import Loader from '../../utils/Loader/Loader';
+import { onSnapshot, collection } from 'firebase/firestore';
+import { db } from '../../utils/FirebaseJson/configFirebase';
 
 const Ubicaciones = () => {
-  const [ubicaciones, setUbicaciones] = useState(null); 
-  const [isLoading, setIsLoading] = useState(true); 
-
-  const getUbicaciones = () => {
-    obtenerUbicaciones()
-      .then((response) => {
-        setUbicaciones(response);
-        setIsLoading(false); 
-      })
-      .catch((error) => {
-        console.error('Error al obtener las ubicaciones:', error);
-      });
-  };
+  const [ubicaciones, setUbicaciones] = useState([]);
 
   useEffect(() => {
-    getUbicaciones();
+    const unsubscribe = onSnapshot(collection(db, 'ubicacion'), (snapshot) => {
+      const ubicacionesData = snapshot.docs.map((doc) => doc.data());
+      setUbicaciones(ubicacionesData);
+    });
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
-    <div className="ubicaciones">
-      {isLoading ? (<Loader />) : (
-        <>
-          <h1 className="animate__animated animate__pulse title-container">Ubicaciones</h1>
-          <div className="ubicaciones-list-container">
-            <div className="ubicaciones-list">
-              <div className="ubicaciones-list-header">
-                <div className='ubicacion-estado'>Estado</div>
-                <p>Nombre</p>
-                <p>Latitud</p>
-                <p>Longtitud</p>
-              </div>  
-              <div className="ubicaciones-list-elems">
-                {ubicaciones &&
-                  ubicaciones.map((ubicacion) => (
-                    <Ubicacion key={ubicacion.nombreDeLaUbicacion} ubicacion={ubicacion} />
-                  ))}
-              </div>
-            </div>
-          </div>
-          <GoBack />
-        </>
-      )}
+    <div>
+      <h1>Ubicaciones</h1>
+      <ul>
+        {ubicaciones.map((ubicacion, index) => (
+          <li key={index}>
+            Nombre: {ubicacion.nombre} | Estado de Alerta: {ubicacion.alerta}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
 
 export default Ubicaciones;
